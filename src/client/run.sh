77bed29662033b0d584151413f1f9519a753ebf2
Swapnil Bhosale <swapnilbhosale@yandex.com>
2019-10-04 15:30:24 -0400

    remove shell scripts

--- src/client/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf *.o client
-g++ -std=c++11 -c client.cpp -lpthread
-g++ -o client client.o -lpthread
-
-./client
-
--- src/server/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf *.o
-rm -f Server
-g++ -g -lpthread -std=c++11 -c BankServer.cpp ServerSock.cpp ObserverPattern.cpp ServerGUI.cpp `pkg-config gtkmm-3.0 --cflags --libs`
-g++ -g -o Server BankServer.o ServerSock.o ObserverPattern.o ServerGUI.o -lpthread  `pkg-config gtkmm-3.0 --cflags --libs`
-echo "Running program now."
-./Server
