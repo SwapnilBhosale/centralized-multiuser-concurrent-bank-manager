81fc41284da3ea3775045813048685d1669c181e
Swapnil Bhosale <swapnil.bhosale@umbc.edu>
2019-11-15 20:34:19 -0500

    add python file to execute programs

--- Clock_Sync/helper.py
+++ Clock_Sync/helper.py
@@ -1,36 +1,45 @@
-import subprocess
+from subprocess import run, Popen
 import sys
+import os
 #subprocess.run(["ls", "-l"])
 
 causal_run =["./Causal "]
 messages = ["Clock", "Multicast", "Commit"]
-drift = [50, -20, 14]
-N = 4
+drift = ["50", "-20", "14"]
+N = "4"
 
 def run_program(args):
+    if(len(args) != 1):
+        print("Please pass one of the option.")
+        print("Supported options are: causal, noncausal, berkeley, bonus")
+        exit(1)
     arg = args[0]
     if arg == "causal":
         for i in range(3):
             cmd = create_causal_command(i)
-            subprocess.run(cmd, shell=True)
+            #Popen(cmd, creationflags = CREATE_NEW_CONSOLE)
+            os.system(" ".join(cmd))
     elif arg == "berkeley":
         for i in range(3):
             cmd = create_berkeley_command(i)
-            subprocess.run(cmd, shell=True)
+            os.system(" ".join(cmd))
         server_opt = ["./Berkeley", "-t", "0", "-s", "server"]
-        subprocess.run(server_opt, shell=True)
+        os.system(" ".join(server_opt))
     elif arg == "noncausal":
         for i in range(3):
             cmd = create_noncausal_command(i)
-            subprocess.run(cmd, shell=True)
-    elif args == "bonus":
+            os.system(" ".join(cmd))
+    elif arg == "bonus":
         for i in range(4):
             cmd = create_dme_command(i, N)
-            subprocess.run(cmd, shell=True)
+            os.system(" ".join(cmd))
+    else:
+    	print("invalid option. Only options allowed are causal, noncausal, berkeley, bonus")
 
 
 def create_berkeley_command(i):
     command = []
+    command.append("gnome-terminal -x")
     command.append("./Berkeley")
     command.append("-t")
     command.append(drift[i])
@@ -38,27 +47,30 @@ def create_berkeley_command(i):
 
 def create_causal_command(i):
     command = []
+    command.append("gnome-terminal -x")
     command.append("./Causal")
     command.append("-i")
-    command.append(i)
+    command.append(str(i))
     command.append("-m")
     command.append(messages[i])
     return command
 
 def create_noncausal_command(i):
     command = []
+    command.append("gnome-terminal -x")
     command.append("./NonCausal")
     command.append("-i")
-    command.append(i)
+    command.append(str(i))
     command.append("-m")
     command.append(messages[i])
     return command
 
 def create_dme_command(i, n):
     command = []
+    command.append("gnome-terminal -x")
     command.append("./DistributedME")
     command.append("-i")
-    command.append(i)
+    command.append(str(i))
     command.append("-n")
     command.append(n)
     return command
